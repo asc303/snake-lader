51336d4d9397ef2e37046e9065b15328f3316938
asc303 <chauhansam94@gmail.com>
2021-11-10 22:56:49 +0530

    [ashutosh] add:DieRolling

--- SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
@@ -6,8 +6,12 @@ namespace SnakeAndLadder
 {
     public class SnakeAndLadder
     {
-       
         public int playerPosition = 0;
-       
+        Random random = new Random();
+        public int DieRolling()
+        {
+            int roll = random.Next(1, 7);
+            return roll;
         }
     }
+}
