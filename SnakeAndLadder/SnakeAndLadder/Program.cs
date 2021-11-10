4cbbb1814d9b30e069eae8dff31ea6ebd73a2a19
asc303 <chauhansam94@gmail.com>
2021-11-10 15:34:08 +0530

    [ashutosh] add:snake&lader class added

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Welcome TO SNAKE AND LADDER Game");
+                SnakeAndLadder playerOne = new SnakeAndLadder();
+                playerOne.DieRolling();
+                playerOne.PlayerCheck();
+            }
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    public class SnakeAndLadder
+    {
+       
+        public int playerPosition = 0;
+       
+        }
+    }
